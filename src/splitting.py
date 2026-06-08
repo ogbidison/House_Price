5dbcbf768b12366bd22cd94b0e536284b8aedbcb
Eric Ogbidi <ogbidison@gmail.com>
2026-06-08 05:13:35 +0100

    Small changes made here

--- src/splitting.py
+++ src/splitting.py
@@ -6,11 +6,12 @@ class split:
         self.test_size = test_size,
         self.random_state = random_state
 
-    def prep_df (df, target_col):
 
-        features = df.drop(columns=[target_col])
-        target = df[target_col]
+    def prep_df (self, data, target_col):
+
+        features = data.drop(columns=[target_col])
+        target = data[target_col]
         #missing = features[miss]
         X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=self.test_size, random_state=self.random_state)
-    return X_train, X_test, y_train, y_test
+        return X_train, X_test, y_train, y_test
    
